0d7d46596e64f1ce9e2312d18e86e0f2badb43d7
Will <wjeckert15@gmail.com>
2020-11-16 16:30:28 -0600

    fixed bugs

--- script.js
+++ script.js
@@ -8,28 +8,22 @@ function main() {
         <button class="shopping-item-toggle"> <span class="button-label">check</span> </button>
         <button class="shopping-item-delete"><span class="button-label">delete</span></button></div></li>`);
 
-        itemDelete();
-        itemChecked();
 
     })
-    itemDelete();
-    itemChecked();
 
-}
-
-function itemDelete(){
-    $('.shopping-item-delete').on('click',  function(event){
+    
+    $('ul').on('click','.shopping-item-delete',  function(event){
 
         $(event.currentTarget).closest("li").remove();
         
     })
+    $(`ul`).on('click','.shopping-item-toggle', function(event){
+        $(event.currentTarget).closest('li').find('.shopping-item').toggleClass('shopping-item__checked')
+    })
+
 }
 
-function itemChecked() {
-    $(`.shopping-item-toggle`).on('click', function(event){
-        $(event.currentTarget).closest('.shopping-list li').toggleClass('shopping-item__checked')
-    })
-}
+
 
 
 $(main);
